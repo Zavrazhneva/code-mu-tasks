12da4b7951588bfb6d2fe131216882e29453057a
Zavrazhneva <zavrazhneva.a@gmail.com>
2020-07-23 23:46:42 +0300

    add parcel

--- index.js
+++ index.js
@@ -11,34 +11,33 @@ function mathCalc1() {
             return input.value;
         });
         const [a,b,c] = inputsValue;
-        console.log(inputsValue)
 
         const isEmpty = inputsValue.some((element) => element === '');
         if(isEmpty) {
             span.innerHTML ='change number';
             return;
         }
-            if(a!==0) {
-                const D = (b**2) -4*a*c;
-                if(D < 0) {
-                    span.innerHTML ='Корней нет';
-                    return;
-                }
-                if(D===0) {
-                    const x = (-b + Math.sqrt(D) ) / 2*a;
-                    span.innerHTML = x;
-                }
-                if(D > 0) {
-                    const x = (-b + Math.sqrt(D) ) / 2*a;
-                    const x2 = (-b - Math.sqrt(D) ) / 2*a;
-                    span.innerHTML = `${x} ${x2}`;
-                }
+        if(a!==0) {
+            const D = (b**2) -4*a*c;
+            if(D < 0) {
+                span.innerHTML ='Корней нет';
+                return;
             }
-            else {
-                span.innerHTML ='a == 0';
+            if(D===0) {
+                const x = (-b + Math.sqrt(D) ) / 2*a;
+                span.innerHTML = x;
+            }
+            if(D > 0) {
+                const x = (-b + Math.sqrt(D) ) / 2*a;
+                const x2 = (-b - Math.sqrt(D) ) / 2*a;
+                span.innerHTML = `${x} ${x2}`;
             }
         }
-
-
+        else {
+            span.innerHTML ='a == 0';
+        }
+    }
 }
 mathCalc1();
+
+//prettier
