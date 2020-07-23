355382626b209a5b5d24650cc8583d10343d9312
Zavrazhneva <zavrazhneva.a@gmail.com>
2020-07-23 22:31:15 +0300

    refactor mathCalc1

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 function mathCalc1() {
-    const form = document.querySelector('.form1');
-    const [a, b, c ] = document.querySelectorAll('.form1 input');
+    const inputs = [...document.querySelectorAll(' input')];
     const span = document.querySelector('#result');
 
-    form.addEventListener('submit', (event)=> {
-        event.preventDefault();
-        if(+a.value!==0) {
-            const D = (b.value**2) -4*a.value*c.value;
-            if(D < 0) {
-                span.innerHTML ='Корней нет';
-                return;
-            }
-            if(D===0) {
-                const x = (-b.value + Math.sqrt(D) ) / 2*a.value;
-                span.innerHTML = x;
-            }
-            if(D > 0) {
-                const x = (-b.value + Math.sqrt(D) ) / 2*a.value;
-                const x2 = (-b.value - Math.sqrt(D) ) / 2*a.value;
-                span.innerHTML = `${x} ${x2}`;
-            }
-        }
-        else {
-            span.innerHTML ='a == 0';
-        }
+    inputs.forEach(element => {
+        element.addEventListener('change', onChangeInput)
     })
+
+    function onChangeInput() {
+        const inputsValue = inputs.map((input) => {
+            return input.value;
+        });
+        const [a,b,c] = inputsValue;
+        console.log(inputsValue)
+
+        const isEmpty = inputsValue.some((element) => element === '');
+        if(isEmpty) {
+            span.innerHTML ='change number';
+            return;
+        }
+            if(a!==0) {
+                const D = (b**2) -4*a*c;
+                if(D < 0) {
+                    span.innerHTML ='Корней нет';
+                    return;
+                }
+                if(D===0) {
+                    const x = (-b + Math.sqrt(D) ) / 2*a;
+                    span.innerHTML = x;
+                }
+                if(D > 0) {
+                    const x = (-b + Math.sqrt(D) ) / 2*a;
+                    const x2 = (-b - Math.sqrt(D) ) / 2*a;
+                    span.innerHTML = `${x} ${x2}`;
+                }
+            }
+            else {
+                span.innerHTML ='a == 0';
+            }
+        }
+
+
 }
 mathCalc1();
