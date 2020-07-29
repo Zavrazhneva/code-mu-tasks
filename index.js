0e749158160e909abf97d54bd9541b981d15cced
Zavrazhneva <zavrazhneva.a@gmail.com>
2020-07-29 10:21:18 +0300

    add task2, task3, task4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function mathCalc1() {
-  const inputs = [...document.querySelectorAll(' input')];
+  const inputs = [...document.querySelectorAll('.taskBody1 input')];
   const span = document.querySelector('#result');
 
   inputs.forEach((element) => {
@@ -39,3 +39,71 @@ function mathCalc1() {
   }
 }
 mathCalc1();
+
+function mathCalc2() {
+  const inputs = [...document.querySelectorAll('.taskBody2 input')];
+  const span = document.querySelector('#result2');
+  inputs.forEach((element) => {
+    element.addEventListener('change', onChangeInput);
+  });
+  function onChangeInput() {
+    const inputsValue = inputs.map((input) => {
+      return input.value;
+    });
+    const [a, b, c] = inputsValue;
+    const sum = a ** 2 + b ** 2 === c ** 2;
+    const isEmpty = inputsValue.some((element) => element === '');
+    if (isEmpty) {
+      span.innerHTML = 'change number';
+    } else if (sum) {
+      span.innerHTML = `Это Пифагорова тройка`;
+    } else {
+      span.innerHTML = `Это не Пифагорова тройка`;
+    }
+  }
+}
+mathCalc2();
+
+function mathCalc3() {
+  const input = document.querySelector('.taskBody3 input');
+  const span = document.querySelector('#result3');
+  const form = document.querySelector('.taskBody3');
+
+  form.addEventListener('submit', onChangeInput);
+  function onChangeInput(event) {
+    event.preventDefault();
+    const divider = [];
+    for (let i = 0; i <= input.value; i++) {
+      if (input.value % i == 0) {
+        divider.push(i);
+      }
+    }
+    span.innerHTML = divider.join(',');
+  }
+}
+mathCalc3();
+
+function mathCalc4() {
+  const inputs = [...document.querySelectorAll('.taskBody4 input')];
+  const span = document.querySelector('#result4');
+  const form = document.querySelector('.taskBody4');
+
+  form.addEventListener('submit', onChangeInput);
+
+  function onChangeInput(event) {
+    event.preventDefault();
+    const inputsValue = inputs.map((input) => {
+      return input.value;
+    });
+    const [a, b] = inputsValue;
+
+    const divider = [];
+    for (let i = 0; i <= Math.max(a, b); i++) {
+      if (a % i == 0 && b % i == 0) {
+        divider.push(i);
+      }
+    }
+    span.innerHTML = divider.join(',');
+  }
+}
+mathCalc4();
